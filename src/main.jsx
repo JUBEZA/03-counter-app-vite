0e300135ec9735ad3719f40c0de287a8357241fa
Juan Bezanilla <juanbezanillaz@gmail.com>
2022-12-28 16:54:10 -0300

    tarea

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom/client'
-import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
+// import { HelloWorldApp } from './HelloWorldApp';
 
 
 ReactDom.createRoot( document.getElementById('root') ).render(
   <React.StrictMode>
-    <HelloWorldApp />
+    <FirstApp />
   </React.StrictMode>
 );
