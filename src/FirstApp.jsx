98fe72eb8cef6ff8fb552fd2f2d4ea06dfb123ca
Juan Bezanilla <juanbezanillaz@gmail.com>
2022-12-28 19:26:19 -0300

    42 - DefaultProps

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -7,11 +7,18 @@ export const FirstApp = ({ title, subtitle }) => {
     <>
      <h1>{ title } </h1>
      <p>{ subtitle }</p>
+     <p>{ nombre }</p>
     </>
   )
 }
 
-FirstApp.propoTypes = {
+FirstApp.propTypes = {
   title: PropTypes.string.isRequires,
   subtitle: PropTypes.number.isRequired,
+}
+
+FirstApp.defaultsTypes = {
+  nombre: 'Fernando Herrera',
+  subtitle: 'No hay subtítulo',
+  title: 'No hay titulo',
 }
