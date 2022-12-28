ef124b1c6ada2a983512e4b2a899fd558a565185
Juan Bezanilla <juanbezanillaz@gmail.com>
2022-12-28 19:39:56 -0300

    43 - Tarea

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom/client'
-import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
+// import { FirstApp } from './FirstApp';
+
 import './styles.css';
 // import { HelloWorldApp } from './HelloWorldApp';
 
 
 ReactDom.createRoot( document.getElementById('root') ).render(
   <React.StrictMode>
-    <FirstApp title="Hola soy Goku" subtitle="Un Subtitulo"/>
+    <CounterApp value = { 10 }/>
   </React.StrictMode>
 );
+
+CounterApp.prototype{
+  value: PropTypes.number.isRequired
+}
