4363cadbfba74209208b5561144e7f9b7a50a866
Juan Bezanilla <juanbezanillaz@gmail.com>
2022-12-29 13:41:17 -0300

    46 - +handleSubstracct y handleResset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -10,14 +10,17 @@ export const CounterApp = ({ value }) => {
       // setCounter( (c) => c + 1)
   }
 
+  const handleSudstract = () => setCounter( counter - 1);
+  const handleReset = () => setCounter( value );
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2> { counter } </h2>
 
-      <button onClick={ handleAdd }>
-        +1
-      </button>
+      <button onClick={ handleAdd }> +1 </button>
+      <button onClick={ handleSudstract }> -1 </button>
+      <button onClick={ handleReset }> Reset </button>
     </>
   );
 }
